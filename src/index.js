92c1aab36f6b727bd5c3badd6675258756a685f7
Venita Kissell <venitak@hotmail.com>
2024-10-27 15:38:07 +1000

    feat: initialise our node enviroment and created simple function

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { niceLogger } = require("./niceLogger");
+
+//**other way to import modules */
+// import the file
+// const niceLoggerFile = require("./niceLogger.js");
+
+// import specific function from the file
+//const niceLoggerFunction = niceLoggerFile.niceLogger;
---
+++ src/niceLogger.js
@@ -0,0 +1,15 @@
+function niceLogger(){
+    console.log("Hello World!");
+    return "Hellow World.";
+}
+
+function sampleTest(){
+    return "Heyy I'm a sample function.";
+}
+
+
+
+module.exports = {
+    niceLogger,
+    sampleTest
+}
